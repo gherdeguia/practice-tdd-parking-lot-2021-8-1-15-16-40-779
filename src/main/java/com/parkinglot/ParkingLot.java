6d2886d6de920edc6211ca43f7b0644faf0cc4c8
GERARLD DE GUIA (DEV-ISD-OOCLL/MNL) <DEGUIGE@OOCL.COM>
2021-08-06 14:49:53 +0800

    [PakingLotExercise][Gerarld] feat : implement code for test should_return_correct_car_when_park_two_cars_given_parking_lot_and_two_parking_tickets

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,23 +1,23 @@
 package com.parkinglot;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
 
     public Car car;
-    private Map<ParkingTicket, Car> parkPosition;
+    private Map<ParkingTicket, Car> parkPosition = new HashMap<>();
 
     public ParkingTicket parkCar(Car car) {
         ParkingTicket parkingTicket = new ParkingTicket();
-        this.car = car;
-        return new ParkingTicket(car);
-
-
+        parkPosition.put(parkingTicket,car);
+        return parkingTicket;
     }
 
 
 
     public Car fetchCar(ParkingTicket parkingTicket) {
-        return this.car;
+        return parkPosition.get(parkingTicket);
+
     }
 }
