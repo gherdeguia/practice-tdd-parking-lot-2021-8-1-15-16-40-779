1566f6c671198ad10f96efdcd72e420b87dded63
GERARLD DE GUIA (DEV-ISD-OOCLL/MNL) <DEGUIGE@OOCL.COM>
2021-08-09 19:09:34 +0800

    [ParkingLotExercise][Gerarld] refactor : organized StandardParkingBoy code & removed redundant code

--- src/main/java/com/parkinglot/StandardParkingBoy.java
+++ src/main/java/com/parkinglot/StandardParkingBoy.java
@@ -1,32 +1,11 @@
 package com.parkinglot;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class StandardParkingBoy extends ParkingBoy{
 
     public StandardParkingBoy(List<ParkingLot> parkingLots){
         super(parkingLots);
-    }
-
-    public ParkingTicket parkCar(Car car) throws NoAvailablePositionException {
-        return findAvailableParkingLot().parkCar(car);
-    }
-
-    protected Car fetchCar(ParkingTicket parkingTicket) {
-        return findWhereParkingLotAndCar(parkingTicket).fetchCar(parkingTicket);
-    }
-
-    public ParkingLot findAvailableParkingLot(){
-        return parkingLots.stream().filter(ParkingLot::hasAvailableParkingSlot).findFirst()
-                .orElseThrow(NoAvailablePositionException::new);
-    }
-
-    public ParkingLot findWhereParkingLotAndCar(ParkingTicket parkingTicket){
-        return parkingLots.stream().filter(parkingLot -> parkingLot.findParkingTicketExists(parkingTicket))
-                .findFirst()
-                .orElseThrow(UnrecognizedParkingTicketException::new);
     }
 
 }
